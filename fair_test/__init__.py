825fc69371b205d8872796f87acb5deea7b089db
Vincent Emonet <vincent.emonet@gmail.com>
2022-09-21 17:33:36 +0200

    upgrade version to 0.0.8

--- fair_test/__init__.py
+++ fair_test/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 from .fair_test_api import FairTestAPI
 from .fair_test_evaluation import FairTestEvaluation
--- fair_test/fair_test.py
+++ fair_test/fair_test.py
@@ -4,6 +4,7 @@ import yaml
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse, PlainTextResponse
 from pydantic import BaseModel
+
 from fair_test import FairTestEvaluation
 from fair_test.config import settings
 
